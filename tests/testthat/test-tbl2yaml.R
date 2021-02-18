de39d54d59951299bdd7326e0e85f232b2a09f63
debruine <debruine@gmail.com>
2021-02-18 19:39:55 +0000

    fixed yaml tests

--- tests/testthat/test-tbl2yaml.R
+++ tests/testthat/test-tbl2yaml.R
@@ -15,7 +15,7 @@ test_that("errors", {
 })
 
 test_that("no table", {
-  y <- tbl2yaml(id = 1:2, fb = "...")
+  y <- tbl2yaml(id = 1:2, fb = "...", open = FALSE)
   comp <- "- id: 1
   fb: '...'
 - id: 2
@@ -23,7 +23,7 @@ test_that("no table", {
 "
   expect_equal(y, comp)
 
-  y <- tbl2yaml(id = 1:2, fb = "...\n")
+  y <- tbl2yaml(id = 1:2, fb = "...\n", open = FALSE)
   comp <- "- id: 1
   fb: |
     ...
@@ -35,8 +35,8 @@ test_that("no table", {
 })
 
 test_that("no filename", {
-  y1 <- tbl2yaml(tbl_path)
-  y2 <- tbl2yaml(tbl)
+  y1 <- tbl2yaml(tbl_path, open = FALSE)
+  y2 <- tbl2yaml(tbl, open = FALSE)
   comp <- "- ID: S1\n  name: Mukul\n  marker: Prof. X\n  question: A\n  KR: 4\n  CE: 4\n  AC: 4\n  mark: 4\n- ID: S2\n  name: Kias\n  marker: Prof. X\n  question: B\n  KR: 4\n  CE: 3\n  AC: 2\n  mark: 3\n- ID: S3\n  name: Omair\n  marker: Prof. X\n  question: B\n  KR: 4\n  CE: 4\n  AC: 4\n  mark: 4\n- ID: S4\n  name: Ramesha\n  marker: Prof. X\n  question: A\n  KR: 1\n  CE: 2\n  AC: 2\n  mark: 1\n- ID: S5\n  name: Libby-Marie\n  marker: Prof. X\n  question: A\n  KR: 2\n  CE: 3\n  AC: 3\n  mark: 2\n"
   expect_equal(y1, comp)
   expect_equal(y2, comp)
@@ -44,7 +44,7 @@ test_that("no filename", {
 
 
 test_that("ok", {
-  expect_silent(tbl2yaml(tbl, filename))
+  expect_silent(tbl2yaml(tbl, filename, open = FALSE))
   expect_true(file.exists(filename_yml))
   tbl2 <- read_marks(yaml = filename_yml) %>% as.data.frame()
   expect_equal(tbl, tbl2)
@@ -54,7 +54,7 @@ test_that("ok", {
 
 
 test_that("add cols", {
-  expect_silent(tbl2yaml(tbl, filename, class = "PSYCH"))
+  expect_silent(tbl2yaml(tbl, filename, class = "PSYCH", open = FALSE))
   expect_true(file.exists(filename_yml))
   tbl2 <- read_marks(yaml = filename_yml) %>% as.data.frame()
   expect_true("class" %in% names(tbl2))
@@ -63,7 +63,7 @@ test_that("add cols", {
 })
 
 test_that("overwrite cols", {
-  expect_warning(tbl2yaml(tbl, filename, marker = "LISA"),
+  expect_warning(tbl2yaml(tbl, filename, marker = "LISA", open = FALSE),
                  "Overwriting column marker", fixed = TRUE)
   expect_true(file.exists(filename_yml))
   tbl2 <- read_marks(yaml = filename_yml) %>% as.data.frame()
